e1bb927e51a70efd9f0e3bfa0d6eb40b6ec93e96
InadzeIlyaM <inadze@yahoo.com>
2022-04-25 07:24:01 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -31,5 +31,25 @@ public class Converter {
         System.out.println("120 rubles are " + dollar + " dollar.");
         System.out.println("5 euro are " + euroRuble + " rubles.");
         System.out.println("6 dollar are " + dollarRuble + " rubles.");
+        float inRuToEuro = 140;
+        float expectedRuToEuro = 2;
+        float outRuToEuro = Converter.rubleToEuro(inRuToEuro);
+        boolean passedRuToEuro = expectedRuToEuro == outRuToEuro;
+        System.out.println("140 rubles are 2 euro. Test result : " + passedRuToEuro);
+        float inRuToDollar = 120;
+        float expectedRuToDollar = 2;
+        float outRuToDollar = Converter.rubleToDollar(inRuToDollar);
+        boolean passedRuToDollar = expectedRuToDollar == outRuToDollar;
+        System.out.println("120 rubles are 2 dollar. Test result : " + passedRuToDollar);
+        float inEuroToRuble = 2;
+        float expectedEuroToRuble = 140;
+        float outEuroToRuble = Converter.euroToRuble(inEuroToRuble);
+        boolean passedEuroToRuble = expectedEuroToRuble == outEuroToRuble;
+        System.out.println("2 euro are 140 rubles . Test result : " + passedEuroToRuble);
+        float inDollarToRuble = 2;
+        float expectedDollarToRuble = 140;
+        float outDollarToRuble = Converter.dollarToRuble(inDollarToRuble);
+        boolean passedDollarToRuble = expectedDollarToRuble == outDollarToRuble;
+        System.out.println("2 dollar are 120 rubles . Test result : " + passedDollarToRuble);
     }
 }
