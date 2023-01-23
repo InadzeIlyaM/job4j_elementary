76d59145790238fd60eeb9becaaa3372f0444f55
InadzeIlyaM <inadze@yahoo.com>
2023-01-23 12:53:53 +0400

    6.9. String - это массив [#504884 #383787]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,30 +2,33 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean valid = false;
-        if (!name.isEmpty() && !(Character.isDigit(name.charAt(0))) && !(Character.isUpperCase(name.charAt(0)))) {
-            for (int i = 1; i < name.length(); i++) {
-                int code = name.codePointAt(i);
-                if (isSpecialSymbol(code)
-                        || isUpperLatinLetter(code)
-                        || isLowerLatinLetter(code)
-                        || Character.isDigit(code)) {
-                    valid = true;
-                }
+        boolean valid = true;
+        if (name.isEmpty() || (Character.isDigit(name.charAt(0))) || Character.isUpperCase(name.charAt(0))) {
+            return false;
+        }
+        for (int i = 1; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (isNotSpecialSymbol(code)
+                    && isNotUpperLatinLetter(code)
+                    && isNotLowerLatinLetter(code)
+                    && !Character.isDigit(code)) {
+                valid = false;
+                break;
             }
         }
+
         return valid;
     }
 
-    public static boolean isSpecialSymbol(int code) {
-        return code == 36 || code == 95;
+    public static boolean isNotSpecialSymbol(int code) {
+        return code != 36 && code != 95;
     }
 
-    public static boolean isUpperLatinLetter(int code) {
-        return 65 <= code && code <= 90;
+    public static boolean isNotUpperLatinLetter(int code) {
+        return 65 > code || code > 90;
     }
 
-    public static boolean isLowerLatinLetter(int code) {
-        return 97 <= code && code <= 122;
+    public static boolean isNotLowerLatinLetter(int code) {
+        return 97 > code || code > 122;
     }
 }
